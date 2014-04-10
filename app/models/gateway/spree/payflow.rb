13e18a63b913e802d494d904c06282ec33b560b2
Timothy Johnson <jhnsntmthy@gmail.com>
2014-04-10 11:58:54 -0400

    Update payflow.rb

--- app/models/gateway/spree/payflow.rb
+++ app/models/gateway/spree/payflow.rb
@@ -1,9 +1,11 @@
-class Gateway::Payflow < Gateway
-  preference :login, :string
-  preference :password, :password
-  preference :partner, :string
+module Spree
+  class Gateway::Payflow < Gateway
+    preference :login, :string
+    preference :password, :password
+    preference :partner, :string
 
-  def provider_class
-    ActiveMerchant::Billing::PayflowGateway
+    def provider_class
+      ActiveMerchant::Billing::PayflowGateway
+    end
   end
 end
